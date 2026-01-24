a5adf6f2cc748e227a4ab2dc1a4098a3900f3599
Aswinmcw <azayasankaran@tenstorrent.com>
2026-01-24 09:09:43 +0000

    Bump version to 2.1.0

--- src/workflow_compare/__init__.py
+++ src/workflow_compare/__init__.py
@@ -2,7 +2,7 @@
 Workflow Compare - A tool to compare GitHub workflow runs
 """
 
-__version__ = "2.0.1"
+__version__ = "2.1.0"
 
 from .api import GitHubAPI
 from .comparator import WorkflowComparator
